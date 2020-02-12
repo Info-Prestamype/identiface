ee62245d61d7a6b913bf00256990b7336235685c
Diego Rodrigo <Diego@diego.local>
2020-02-12 16:46:04 -0500

    fix demo

---
+++ example/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Prestamype WebCam"/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/example.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
